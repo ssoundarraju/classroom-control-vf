dc122d03da97cda755601fd200d6a484348252a7
ssoundarraju <titanides4@gmail.com>
2016-06-29 08:31:36 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,13 +2,13 @@ class skeleton{
 file{ '/etc/skel':
  ensure => directory,
  owner =>'root',
- group => 'root'
+ group => 'root',
  mode => '0755',
 }
 file{'/etc/skel/.bashrc':
 ensure => file
  owner =>'root',
- group => 'root'
+ group => 'root',
  mode => '0644',
  source => 'puppet:///modules/skeleton/bashrc',
 }
